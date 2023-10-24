a934e77dbeb9509c0bb803dcb31270f72f207854
lucasrossi0 <lucasrossi502@gmail.com>
2023-10-24 11:23:10 -0300

    Iniciando no projeto e tentando

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+let startBtn = document.getElementById("start");
+let stopBtn = document.getElementById("stop");
+let restartBtn = document.getElementById("restart");
+let timeDisplay = document.getElementById("time");
+
+// click no start, o display comeca a rodar
+// click no stop, o display para de rodar
+// click no restart, o display volta ao 0
+
+// funcao para iniciar o tempo
+// funcao para parar o tempo
+// funcao para zerar o tempo
+
+startBtn.addEventListener("click", () => {});
